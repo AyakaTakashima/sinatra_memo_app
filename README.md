c7e9bc8ad1762638aa00c91b6ebd203da18dfdab
TakashimaAyaka <takashimaayaka@Ayaka-macbook-air.local>
2022-02-02 20:40:58 +0900

    readmeにddl文を追加&dbへの接続を使い回すようにした

--- README.md
+++ README.md
@@ -14,14 +14,31 @@ $ git clone https://github.com/自分のアカウント名/fjord-sinatra_memo_ap
 ### アプリケーションを立ち上げのために行う操作
 以下の手順に従って環境セットアップを実行してください。
 1. `bundle init`を実行
-2. 生成されたGemfileに下記の2行を追記
+2. 生成されたGemfileに下記の3行を追記
 ```
-gem "sinatra"
-gem "webrick"
+gem 'sinatra'
+gem 'pg'
+gem 'webrick'
 ```
 3. `bundle install` を実行
-4. `bundle exec ruby sinatra_memo_app.rb`を実行
-5. `http://127.0.0.1:4567/memo`にアクセス
+4. 下記コマンドにてpostgersでログイン
+```
+$ psql postgres
+```
+5. データベースを作成
+```
+create database web_application_practice;
+```
+6. `-d`オプションで`web_application_practice`データベースへ入り、下記のコマンドを実行
+```
+create table memo_data
+(id serial
+,title text
+,text text
+,primary key(id));
+```
+7. `bundle exec ruby sinatra_memo_app.rb`を実行
+8. `http://127.0.0.1:4567/memo`にアクセス
 
 
 
--- sinatra_memo_app.rb
+++ sinatra_memo_app.rb
@@ -7,36 +7,33 @@ require 'pg'
 
 set :environment, :production
 
+connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+
 # This is class to read memo data from database.
 class Memo
-  def self.all
-    connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+  def self.all(connection)
     connection.exec('SELECT * FROM memo_data ORDER BY id')
   end
 
-  def self.create_new(title, text)
-    connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+  def self.create_new(connection, title, text)
     connection.exec("INSERT INTO memo_data(title, text) VALUES ('#{title}','#{text}')")
   end
 
-  def self.show(id)
-    connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+  def self.show(connection, id)
     connection.exec("SELECT id, title, text FROM memo_data WHERE id = #{id}") { |result| result[0] }
   end
 
-  def self.update(id, title, text)
-    connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+  def self.update(connection, id, title, text)
     connection.exec("UPDATE memo_data SET title = '#{title}', text = '#{text}' WHERE id = #{id}")
   end
 
-  def self.delete(id)
-    connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+  def self.delete(connection, id)
     connection.exec("DELETE FROM memo_data WHERE id = #{id}")
   end
 end
 
 get '/memo' do
-  @memo_data = Memo.all
+  @memo_data = Memo.all(connection)
   erb :top_page
 end
 
@@ -47,38 +44,38 @@ end
 get '/memo/:id' do
   memo_id = params[:id].to_i
 
-  @memo_data = Memo.show(memo_id)
+  @memo_data = Memo.show(connection, memo_id)
 
   erb :memo_page
 end
 
 get '/memo/:id/edit' do
   memo_id = params[:id].to_i
-  @memo_data = Memo.show(memo_id)
+  @memo_data = Memo.show(connection, memo_id)
 
   erb :edit_page
 end
 
 post '/memo' do
-  title = params[:title].gsub("'","''")
-  text = params[:text].gsub("'","''")
-  Memo.create_new(title, text)
+  title = params[:title].gsub("'", "''")
+  text = params[:text].gsub("'", "''")
+  Memo.create_new(connection, title, text)
 
   redirect to('/memo')
 end
 
 patch '/memo/:id' do
   memo_id = params[:id].to_i
-  title = params[:title].gsub("'","''")
-  text = params[:text].gsub("'","''")
-  Memo.update(memo_id, title, text)
+  title = params[:title].gsub("'", "''")
+  text = params[:text].gsub("'", "''")
+  Memo.update(connection, memo_id, title, text)
 
   redirect to('/memo')
 end
 
 delete '/memo/:id' do
   memo_id = params[:id].to_i
-  Memo.delete(memo_id)
+  Memo.delete(connection, memo_id)
 
   redirect to('/memo')
 end
