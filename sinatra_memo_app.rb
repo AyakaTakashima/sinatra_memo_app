abb3687f5ca235b9b9aac8ffcff6b98f37f485d1
TakashimaAyaka <takashimaayaka@Ayaka-macbook-air.local>
2022-02-07 20:03:31 +0900

    readmeの不要な行の削除&データベースへの接続を定数にしてクラス内に置いた

--- sinatra_memo_app.rb
+++ sinatra_memo_app.rb
@@ -7,33 +7,33 @@ require 'pg'
 
 set :environment, :production
 
-connection = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
-
 # This is class to read memo data from database.
 class Memo
-  def self.all(connection)
-    connection.exec('SELECT * FROM memo_data ORDER BY id')
+  CONNECTION = PG.connect(host: 'localhost', user: 'takashimaayaka', password: '', dbname: 'web_application_practice')
+
+  def self.all
+    CONNECTION.exec('SELECT * FROM memo_data ORDER BY id')
   end
 
-  def self.create_new(connection, title, text)
-    connection.exec("INSERT INTO memo_data(title, text) VALUES ('#{title}','#{text}')")
+  def self.create_new(title, text)
+    CONNECTION.exec('INSERT INTO memo_data(title, text) VALUES ($1, $2)', [title, text])
   end
 
-  def self.show(connection, id)
-    connection.exec("SELECT id, title, text FROM memo_data WHERE id = #{id}") { |result| result[0] }
+  def self.show(id)
+    CONNECTION.exec('SELECT id, title, text FROM memo_data WHERE id = $1', [id]) { |result| result[0] }
   end
 
-  def self.update(connection, id, title, text)
-    connection.exec("UPDATE memo_data SET title = '#{title}', text = '#{text}' WHERE id = #{id}")
+  def self.update(id, title, text)
+    CONNECTION.exec('UPDATE memo_data SET title = $1, text = $2 WHERE id = $3', [title, text, id])
   end
 
-  def self.delete(connection, id)
-    connection.exec("DELETE FROM memo_data WHERE id = #{id}")
+  def self.delete(id)
+    CONNECTION.exec('DELETE FROM memo_data WHERE id = $1', [id])
   end
 end
 
 get '/memo' do
-  @memo_data = Memo.all(connection)
+  @memo_data = Memo.all
   erb :top_page
 end
 
@@ -44,14 +44,14 @@ end
 get '/memo/:id' do
   memo_id = params[:id].to_i
 
-  @memo_data = Memo.show(connection, memo_id)
+  @memo_data = Memo.show(memo_id)
 
   erb :memo_page
 end
 
 get '/memo/:id/edit' do
   memo_id = params[:id].to_i
-  @memo_data = Memo.show(connection, memo_id)
+  @memo_data = Memo.show(memo_id)
 
   erb :edit_page
 end
@@ -59,7 +59,7 @@ end
 post '/memo' do
   title = params[:title].gsub("'", "''")
   text = params[:text].gsub("'", "''")
-  Memo.create_new(connection, title, text)
+  Memo.create_new(title, text)
 
   redirect to('/memo')
 end
@@ -68,14 +68,14 @@ patch '/memo/:id' do
   memo_id = params[:id].to_i
   title = params[:title].gsub("'", "''")
   text = params[:text].gsub("'", "''")
-  Memo.update(connection, memo_id, title, text)
+  Memo.update(memo_id, title, text)
 
   redirect to('/memo')
 end
 
 delete '/memo/:id' do
   memo_id = params[:id].to_i
-  Memo.delete(connection, memo_id)
+  Memo.delete(memo_id)
 
   redirect to('/memo')
 end
